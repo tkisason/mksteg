8e33a9a5e959dcacaafa300cf7c879e869aa9e89
Tonimir Kisasondi <kisasondi@gmail.com>
2011-05-17 09:54:04 +0200

    Bugfixes, v0.1

--- mksteg.py
+++ mksteg.py
@@ -21,12 +21,19 @@ def entropy(data):
             Hd += - pi * math.log(pi, 2)
     return Hd
 
+def to_hex(input):
+    return ''.join(['%02x' % ord(x) for x in input])
+
+def to_chr(input):
+    return ''.join(['%02s' % chr(int(x,16)) for x in split_n(input,2)])
+
 def rfile(filename):
     return open(filename,"r").read()
 
 def trn(input,fr,to):
     import string
-    return(input.translate(string.maketrans(fr,to)))
+    tr = string.maketrans(fr,to)
+    return input.translate(tr)
 
 def can_dec(cfx,dfx):
     if len(cfx) >= len(dfx):
@@ -63,7 +70,7 @@ def simple_dict_mapper(cfx,dfx):
         mapper[cst[i]] = dst[i]
     return mapper
 
-def simple_mapper(cfx,dfx):
+def simple_tr_mapper(cfx,dfx):
     if can_dec(cfx,dfx) == 0:
         return 0
     cst = get_symb_array(cfx)
@@ -72,18 +79,34 @@ def simple_mapper(cfx,dfx):
     dst = dst[:len(dst)]
     return cst,dst
 
-
-
-cfx = {} # carrier frequency
-dfx = {} # data frequency
-
-carrier = open(argv[1],"r").read()
-data = open(argv[2],"r").read()
-#output = open(argv[3],"w")
-
-cfx = freq_an(carrier,cfx)
-dfx = freq_an(data,dfx)
-
-print simple_mapper(cfx,dfx)
-
+def encode_data(cfx,dfx,data):
+    if len(cfx) <= len(dfx):
+        print "[+] carrier data mismatch!"
+        return 0
+    map = simple_tr_mapper(cfx,dfx)
+    print "[+] map0: ",map[0]
+    cmap = ''.join(map[0])
+    print "[+] cmap: ", to_hex(cmap)
+    print "[+] map1: ",map[1]
+    dmap = ''.join(map[1])
+    print "[+] dmap: ", to_hex(dmap)
+    return trn(data,dmap,cmap)
+    
+if __name__ == '__main__':
+    if len(argv) != 4:
+        print "[+] usage: CARRIERFILE DATAFILE OUTPUTFILE"
+        print "[+] current method: simple probability mapping"
+        quit
+    else:
+        cfx = {} # carrier frequency
+        dfx = {} # data frequency
+        carrier = open(argv[1],"r").read()
+        data = open(argv[2],"r").read()
+        output = open(argv[3],"w")
+        cfx = freq_an(carrier,cfx)
+        dfx = freq_an(data,dfx)
+        out = encode_data(cfx,dfx,data)
+        if out != 0:
+            output.write(out)
+            print "[+] message written to OUTFILE"
    
